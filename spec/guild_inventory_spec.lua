3dec175f4b0aa04ccca6cf5bebf6ed0ccac9de68
Edgardo Carreras <edgardo.g.carreras@gmail.com>
2024-10-19 01:14:05 -0400

    Make test pass

--- spec/guild_inventory_spec.lua
+++ spec/guild_inventory_spec.lua
@@ -1,7 +1,14 @@
+GuildInventory = {};
+
+function GuildInventory:new()
+    local self = {};
+    return self;
+end
+
 describe("GuildInventory", function()
     describe("when adding item for player", function()
         it("can be found inside inventory", function()
             local fellowshipInventory = GuildInventory:new();
-        end);
-    end);
-end);
+        end)
+    end)
+end)
