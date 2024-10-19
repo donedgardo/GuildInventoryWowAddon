306d232b74a1f185e166fe311848ec55751d0b1e
Edgardo Carreras <edgardo.g.carreras@gmail.com>
2024-10-19 00:47:12 -0400

    Add first uncompillable test

---
+++ spec/guild_inventory_spec.lua
@@ -0,0 +1,7 @@
+describe("GuildInventory", function()
+    describe("when adding item for player", function()
+        it("can be found inside inventory", function()
+            local fellowshipInventory = GuildInventory:new();
+        end);
+    end);
+end);
