0a5ec4d937445a8c8fca10c4d4cd9d72c74cb9f0
Edgardo Carreras <edgardo.g.carreras@gmail.com>
2024-10-19 03:04:12 -0400

    Refactor: Remove test duplication

--- spec/guild_inventory_spec.lua
+++ spec/guild_inventory_spec.lua
@@ -1,20 +1,27 @@
 local GuildInventory = require('GuildInventoryTracker')
 
 describe("GuildInventory", function()
-    describe("when adding item for player", function()
-        it("increases in size by one", function()
-            local fellowshipInventory = GuildInventory:new();
-            local theRingId = 123;
-            fellowshipInventory.addItem(theRingId);
-            local items = fellowshipInventory.getItems();
-            assert.True(#items == 1);
-        end)
-        it("has the item added", function()
-            local fellowshipInventory = GuildInventory:new();
-            local theRingId = 123;
-            fellowshipInventory.addItem(theRingId);
-            local items = fellowshipInventory.getItems();
-            assert.are.same(items[1].id, theRingId);
-        end)
+  local fellowshipInventory;
+  before_each(function()
+    fellowshipInventory = GuildInventory:new();
+  end)
+  describe("when adding item for player", function()
+    local theRingId = 123;
+    before_each(function()
+      fellowshipInventory.addItem(theRingId);
     end)
+    describe("getting items", function()
+      local items;
+      before_each(function()
+        items = fellowshipInventory.getItems();
+      end)
+      it("increases in size by one", function()
+        assert.True(#items == 1);
+      end)
+      it("has the item added", function()
+        assert.are.same(items[1].id, theRingId);
+      end)
+    end)
+
+  end)
 end)
